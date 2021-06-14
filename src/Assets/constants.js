da6c7de2ab2ea0cfcc3e5df17ce34427702fe533
uriol98 <oriol1998@gmail.com>
2021-06-14 17:30:31 +0200

    everything developed, but without style. Waiting to find bugs

--- src/Assets/constants.js
+++ src/Assets/constants.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'http://localhost:8080' ;
+export const API_BASE_URL = 'https://finalproject-server.herokuapp.com' ;
 export const ACCESS_TOKEN = 'accessToken';
 
 // server address 'https://finalproject-server.herokuapp.com'
