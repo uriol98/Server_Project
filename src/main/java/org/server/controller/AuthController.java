2df072aff1ab2bc4e4ca687f29cf61cb3b414900
uriol98 <oriol1998@gmail.com>
2021-06-15 15:57:28 +0200

    send reset password email subject changed

--- src/main/java/org/server/controller/AuthController.java
+++ src/main/java/org/server/controller/AuthController.java
@@ -76,7 +76,7 @@ public class AuthController {
     public ResponseEntity<?> sendEmailResetPassword(@Valid @RequestBody  Email email){
 
         String token = userService.generateResetToken(email.email);
-        emailService.sendSimpleMessageResetPassword("oriol1998@gmail.com","test",token);
+        emailService.sendSimpleMessageResetPassword("oriol1998@gmail.com","Zresetuj hasło",token);
         return ResponseEntity.ok("Message sent to you email account succesfully and the token is:"+token);
     }
 
